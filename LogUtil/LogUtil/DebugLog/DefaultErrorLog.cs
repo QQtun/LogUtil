6da2f75b38c4f818f3a42562eb849edbc141e96d
QQtun <deng.chengdun@gmail.com>
2021-11-04 14:42:37 +0800

    修改Console版本顏色與樣式

--- LogUtil/LogUtil/DebugLog/DefaultErrorLog.cs
+++ LogUtil/LogUtil/DebugLog/DefaultErrorLog.cs
@@ -8,6 +8,8 @@ namespace LogUtil
         private const string TimeFormat = "yyyy-MM-dd HH:mm:ss";
         private const string Space = " ";
 
+        private Dictionary<LogTag, string> _tagDic = new Dictionary<LogTag, string>();
+
         public void SetTimeVisible(bool visible)
         {
         }
@@ -30,78 +32,102 @@ namespace LogUtil
 
         public void ShowErrorLog(LogTag tag, object log)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("[E]");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(DateTime.Now.ToString(TimeFormat));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(GetTagString(tag));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(log.ToString());
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public void ShowWarningLog(LogTag tag, object log)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("[W]");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(DateTime.Now.ToString(TimeFormat));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(GetTagString(tag));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(log.ToString());
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public void ShowLog(LogTag tag, object log)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("[I]");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(DateTime.Now.ToString(TimeFormat));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(GetTagString(tag));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(log.ToString());
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public void ShowErrorLogFormat(LogTag tag, string log, params object[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("[E]");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(DateTime.Now.ToString(TimeFormat));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(GetTagString(tag));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(log, args);
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public void ShowWarningLogFormat(LogTag tag, string log, params object[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("[W]");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(DateTime.Now.ToString(TimeFormat));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(GetTagString(tag));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(log, args);
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public void ShowLogFormat(LogTag tag, string log, params object[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("[I]");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write(DateTime.Now.ToString(TimeFormat));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(GetTagString(tag));
             Console.Write(Space);
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(log, args);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private string GetTagString(LogTag tag)
+        {
+            if(!_tagDic.TryGetValue(tag, out var tagStr))
+            {
+                tagStr = string.Format("{0,-10}", tag.ToString());
+                _tagDic.Add(tag, tagStr);
+            }
+            return tagStr;
         }
     }
 }
